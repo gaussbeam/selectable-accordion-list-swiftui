55efa7382cdd390849c6c83a86d7fe975c70505b
gaussbeam <koji.hanyu@studist.jp>
2023-10-12 15:22:48 +0900

    Add files

---
+++ selectable-accordion-list-swiftui/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  selectable-accordion-list-swiftui
+//
+//  Created by HANYU, Koji on 2023/10/12.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ selectable-accordion-list-swiftui/selectable_accordion_list_swiftuiApp.swift
@@ -0,0 +1,17 @@
+//
+//  selectable_accordion_list_swiftuiApp.swift
+//  selectable-accordion-list-swiftui
+//
+//  Created by HANYU, Koji on 2023/10/12.
+//
+
+import SwiftUI
+
+@main
+struct selectable_accordion_list_swiftuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
